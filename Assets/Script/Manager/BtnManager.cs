fcfb9b61c72bfb80c82e70f295757237fba7aac3
전유한 <sdh20200225@sdh.hs.kr>
2022-06-18 22:24:37 +0900

    추가용

---
+++ Assets/Script/Manager/BtnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI
+    ;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour
+{
+    public void OnClick_1Stage_Btn()
+    {
+        Debug.Log("1Stage열림");
+    }
+    public void OnClick_2Stage_Btn()
+    {
+        Debug.Log("2Stage열림");
+    }
+    public void OnClick_3Stage_Btn()
+    {
+        Debug.Log("3Stage열림");
+    }
+
+    public void OnClick_4Stage_Btn()
+    {
+        Debug.Log("4Stage열림");
+    }
+
+}
